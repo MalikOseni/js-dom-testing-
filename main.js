477dd3d0443c28fda3f0e3fe82a215c29e32a472
MalikOseni <hosmaad@gmail.com>
2022-09-23 19:55:30 +0100

    random generator game

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+// let subject = document.createAttribute('div')
 let title = document.createElement('p');
+// subject.appendChild(title);
 title.innerText = "vibe with ann";
 title.classList.add('style')
-document.body.appendChild(title)
+document.body.appendChild(title);
+
+let par = document.querySelector('p');
+par.addEventListener("click", randum);
+
+function randum() {
+    let rand = [];
+    for (let i = 0; i < 5; i++){
+        rand.push(Math.floor(Math.random(i)*10))
+    }
+    par.textContent =`${rand}`
+    }
